e108b05053308bfecac65b30f77589179e8c6ccc
Ilnuraxon <tventi96@mail.ru>
2024-05-30 21:51:00 +0500

    upgrade schemas

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class STaskId(BaseModel):
     ok: bool = True
@@ -13,3 +13,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+    
+
+    model_config = ConfigDict(from_attributes=True)
+
+
